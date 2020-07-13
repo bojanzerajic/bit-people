1d346c2c4dd9f11285e11d825fb7719624de0ab7
Zeraja <zerajicbojan@gmail.com>
2020-07-13 14:17:06 +0200

    Updates

---
+++ precache-manifest.6e2eae3968c36da2932f4ad649c835d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde14936a02741c4b79413b81f60ae54",
+    "url": "/bit-people/index.html"
+  },
+  {
+    "revision": "44a762bf8dbe989fb940",
+    "url": "/bit-people/static/css/main.35f73aac.chunk.css"
+  },
+  {
+    "revision": "535390bb19a18b1ec402",
+    "url": "/bit-people/static/js/2.b4ddc029.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bit-people/static/js/2.b4ddc029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44a762bf8dbe989fb940",
+    "url": "/bit-people/static/js/main.32ae205e.chunk.js"
+  },
+  {
+    "revision": "e0827626558f33879c68",
+    "url": "/bit-people/static/js/runtime-main.500e00d3.js"
+  }
+]);
